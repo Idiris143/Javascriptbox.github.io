a48680d6d8922f3de90816092c19b2e5ced29735
Idiris Jaahwayn <58189828+Idiris143@users.noreply.github.com>
2023-10-17 02:23:33 +0300

    second commit

---
+++ dom.js
@@ -0,0 +1,26 @@
+const btnOne = document.querySelector('.button-1');
+const btnTwo = document.querySelector('.button-2');
+const btnThree = document.querySelector('.button-3');
+const btnFour = document.querySelector('.button-4');
+
+btnOne.addEventListener('click', () => {
+    document.body.style.background = 'lightblue';
+
+})
+btnTwo.addEventListener('click', () => {
+    document.body.style.background = 'orange';
+
+})
+btnThree.addEventListener('click', () => {
+    document.body.style.background = 'lightgrey';
+
+})
+btnFour.addEventListener('click', () => {
+    document.body.style.background = 'lightcoral';
+
+})
+
+
+
+
+
